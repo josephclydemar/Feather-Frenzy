6566a7ef0a1250af591e6f105af150b17bcd4b8b
Clyde Mar <clydemar234@gmail.com>
2024-09-22 20:30:24 +0800

    feat: Add initial code to open window

---
+++ include/common.hpp
@@ -0,0 +1,13 @@
+
+
+#ifndef COMMON_H
+#define COMMON_H
+
+
+#define WINDOW_WIDTH                        1200
+#define WINDOW_HEIGHT                       600
+
+
+
+#endif
+
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include "raylib.h"
+#include "common.hpp"
+
+int main(void) {
+
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Tetra Zen");
+    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+
+
+    // Main game loop
+    while(!WindowShouldClose()) {       // Detect window close button or ESC key
+        
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        EndDrawing();
+    }
+    CloseWindow();                      // Close window and OpenGL context
+    return 0;
+}
