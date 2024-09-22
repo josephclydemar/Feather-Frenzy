a9e2921df2203e9a2ddd2fe45c50cf702e19a761
Clyde Mar <clydemar234@gmail.com>
2024-09-23 00:23:29 +0800

    feat(player): Draw Player to window

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include "raylib.h"
 #include "common.hpp"
+#include "player.hpp"
 
 int main(void) {
 
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Feather Frenzy");
     SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+
+    Player* p1 = new Player(50.5f, 50.0f, RED);
 
 
     // Main game loop
@@ -12,7 +15,7 @@ int main(void) {
         
         BeginDrawing();
         ClearBackground(BLACK);
-
+        p1->Draw();
         EndDrawing();
     }
     CloseWindow();                      // Close window and OpenGL context
