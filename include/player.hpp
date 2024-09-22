b48aae3c2295067346350e1c537b5500c8d67764
Clyde Mar <clydemar234@gmail.com>
2024-09-23 00:22:55 +0800

    feat(player): Add initial Player class with initial Draw method

---
+++ include/player.hpp
@@ -0,0 +1,20 @@
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "raylib.h"
+
+class Player {
+    private:
+        Vector2 pos;
+        Vector2 size;
+        Vector2 body[6];
+        Color color;
+
+    public:
+        Player(float posX, float posY, Color color);
+        void Draw(void);
+};
+
+#endif
+
---
+++ src/player.cpp
@@ -0,0 +1,32 @@
+#include "player.hpp"
+
+Player::Player(float posX, float posY, Color color) {
+    this->pos.x = posX;
+    this->pos.y = posY;
+    this->color = color;
+
+    this->size.x = 50.0f;
+    this->size.y = 18.0f;
+
+    this->body[0].x = this->pos.x + this->size.x * 0.4f;
+    this->body[0].y = this->pos.y;
+
+    this->body[1].x = this->pos.x + this->size.x * 0.5f;
+    this->body[1].y = this->pos.y - this->size.y * 0.5f;
+
+    this->body[2].x = this->pos.x - this->size.x * 0.2f;
+    this->body[2].y = this->pos.y - this->size.y;
+
+    this->body[3].x = this->pos.x - this->size.x * 0.5f;
+    this->body[3].y = this->pos.y - this->size.y * 0.5f;
+
+    this->body[4].x = this->pos.x - this->size.x * 0.4f;
+    this->body[4].y = this->pos.y;
+
+    this->body[5].x = this->pos.x + this->size.x * 0.4f;
+    this->body[5].y = this->pos.y;
+}
+
+void Player::Draw(void) {
+    DrawSplineLinear(this->body, 6, 1.0f, this->color);
+}
